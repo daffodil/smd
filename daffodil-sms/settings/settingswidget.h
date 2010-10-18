d039e10e664d87ccc7459bb21f3a9e631a392c5c
Peter Mash Morgan <ac001@daffodil.uk.com>
2010-10-18 11:18:43 +0100

    user and docs

---
+++ daffodil-sms/settings/settingswidget.h
@@ -0,0 +1,48 @@
+#ifndef SETTINGSWIDGET_H
+#define SETTINGSWIDGET_H
+
+#include "mainobject.h"
+
+#include <QtCore/QString>
+
+#include <QtGui/QWidget>
+#include <QtGui/QLineEdit>
+#include <QtGui/QStatusBar>
+#include <QtGui/QTreeWidget>
+#include <QtGui/QPushButton>
+#include <QtGui/QGroupBox>
+
+
+class SettingsWidget : public QWidget
+{
+Q_OBJECT
+public:
+    explicit SettingsWidget(MainObject *mOb, QWidget *parent = 0);
+
+private:
+
+    MainObject *mainObject;
+    QGroupBox *grpExecutable;
+    QGroupBox *grpFgRoot;
+    QGroupBox *grpFgScenery;
+
+    QLineEdit *txtExecutable;
+    QLineEdit *txtFgRoot;
+    QTreeWidget *treeFgScenery;
+    QStatusBar *statusBar;
+
+    QPushButton *buttSceneryUp;
+    QPushButton *buttSceneryDown;
+    QPushButton *buttSceneryRemove;
+
+
+    QString set_frame_style(QString color);
+    void disable_scenery_actions(bool);
+signals:
+
+public slots:
+    void on_exe_path();
+    void on_exe_autodetect();
+};
+
+#endif // SETTINGSWIDGET_H
